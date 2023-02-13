69e28b1b2ee396a299d680c87fb9cb6b59c6fd39
Phuwadol Rachbanjoeb <phuwadol.r@arise.tech>
2023-02-13 18:01:14 +0700

    555

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"agolin/controllers"
+	"agolin/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	models.ConnectDataBase()
+
+	r := gin.Default()
+
+	public := r.Group("/api")
+
+	public.POST("/register", controllers.Register)
+
+	r.Run(":8080")
+
+}
